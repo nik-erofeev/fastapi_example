75985c2697d69c9e86e317f15e0863027f85d72c
Erofeev Nikita <erofeev.nik.it@yandex.ru>
2024-02-02 17:58:20 +0300

    вынесли except

--- app/api/users/exceptions.py
+++ app/api/users/exceptions.py
@@ -5,3 +5,9 @@ http_data_conflict_exception = HTTPException(
     status_code=status.HTTP_409_CONFLICT,
     detail="Пользователь с таким именем username/email уже существует",
 )
+
+
+http_user_not_found_exception = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail="Пользователь с таким именем пользователя не найден",
+)
--- app/api/users/utils.py
+++ app/api/users/utils.py
@@ -1,6 +1,6 @@
-from fastapi import HTTPException, status
 from sqlalchemy.exc import NoResultFound
 
+from app.api.users.exceptions import http_user_not_found_exception
 from app.api.users.repository import UserRepository
 from app.api.users.schemas import UserByUsernameRequest
 from app.database import SessionDep
@@ -18,7 +18,4 @@ async def user_by_username(
         return result
 
     except NoResultFound:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Пользователь с указанным именем пользователя не найден",
-        )
+        raise http_user_not_found_exception
